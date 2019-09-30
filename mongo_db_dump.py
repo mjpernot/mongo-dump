f4135b148cc1f4af7c1b8fd5df4af45cb723fbc1
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-09-30 16:14:24 +0100

    Update mongo_db_dump.main v3.0.2 - Refactored "if" statements

--- mongo_db_dump.py
+++ mongo_db_dump.py
@@ -232,14 +232,14 @@ def main():
     # Process argument list from command line.
     args_array = arg_parser.arg_parse2(sys.argv, opt_val_list)
 
-    if not gen_libs.help_func(args_array, __version__, help_message):
-        if not arg_parser.arg_require(args_array, opt_req_list) \
-           and arg_parser.arg_req_xor(args_array, opt_req_xor_list) \
-           and arg_parser.arg_noreq_xor(args_array, xor_noreq_list) \
-           and arg_parser.arg_cond_req(args_array, opt_con_req_list) \
-           and not arg_parser.arg_dir_chk_crt(args_array, dir_chk_list,
-                                              dir_crt_list):
-            run_program(args_array, func_dict, opt_arg=opt_arg_list)
+    if not gen_libs.help_func(args_array, __version__, help_message) \
+       and not arg_parser.arg_require(args_array, opt_req_list) \
+       and arg_parser.arg_req_xor(args_array, opt_req_xor_list) \
+       and arg_parser.arg_noreq_xor(args_array, xor_noreq_list) \
+       and arg_parser.arg_cond_req(args_array, opt_con_req_list) \
+       and not arg_parser.arg_dir_chk_crt(args_array, dir_chk_list,
+                                          dir_crt_list):
+        run_program(args_array, func_dict, opt_arg=opt_arg_list)
 
 
 if __name__ == "__main__":
