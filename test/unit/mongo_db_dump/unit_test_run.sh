b4acbe6ae2a685e793437017f8793728c54a1747
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-12-17 09:35:56 +0000

    Updated test/unit/mongo_db_dump/unit_test_run.sh v3.2.0

--- test/unit/mongo_db_dump/unit_test_run.sh
+++ test/unit/mongo_db_dump/unit_test_run.sh
@@ -10,5 +10,6 @@ test/unit/mongo_db_dump/get_req_options.py
 test/unit/mongo_db_dump/help_message.py
 test/unit/mongo_db_dump/main.py
 test/unit/mongo_db_dump/mongo_dump.py
+test/unit/mongo_db_dump/mongo_generic.py
 test/unit/mongo_db_dump/run_program.py
 test/unit/mongo_db_dump/sync_cp_dump.py
