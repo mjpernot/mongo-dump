dc1cd8ee89b2f3d136c041d315ed90b3652d7e2d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-12-21 14:48:32 +0000

    Updated test/unit/mongo_db_dump/mongo_dump.py v3.2.0

--- test/unit/mongo_db_dump/mongo_dump.py
+++ test/unit/mongo_db_dump/mongo_dump.py
@@ -35,96 +35,6 @@ import version
 __version__ = version.__version__
 
 
-class Mail(object):
-
-    """Class:  Mail
-
-    Description:  Class stub holder for gen_class.Mail class.
-
-    Methods:
-        __init__ -> Class initialization.
-        add_2_msg -> Stub method holder for Mail.add_2_msg.
-        send_mail -> Stub method holder for Mail.send_mail.
-
-    """
-
-    def __init__(self):
-
-        """Method:  __init__
-
-        Description:  Class initialization.
-
-        Arguments:
-
-        """
-
-        self.data = None
-
-    def add_2_msg(self, data):
-
-        """Method:  add_2_msg
-
-        Description:  Stub method holder for Mail.add_2_msg.
-
-        Arguments:
-            (input) data -> Message line to add to email body.
-
-        """
-
-        self.data = data
-
-        return True
-
-    def send_mail(self):
-
-        """Method:  send_mail
-
-        Description:  Stub method holder for Mail.send_mail.
-
-        Arguments:
-
-        """
-
-        return True
-
-
-class SubProcess(object):
-
-    """Class:  SubProcess
-
-    Description:  Class which is a representation of the subprocess class.
-
-    Methods:
-        __init__ -> Initialize configuration environment.
-        wait -> subprocess.wait method.
-
-    """
-
-    def __init__(self):
-
-        """Method:  __init__
-
-        Description:  Initialization instance of the ZipFile class.
-
-        Arguments:
-
-        """
-
-        pass
-
-    def wait(self):
-
-        """Method:  wait
-
-        Description:  Mock representation of subprocess.wait method.
-
-        Arguments:
-
-        """
-
-        pass
-
-
 class Server(object):
 
     """Class:  Server
@@ -198,14 +108,10 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
-        test_mail_log_suppress -> Test with log file and mail and suppression.
-        test_log_file_suppress -> Test with log file with data and suppression.
-        test_empty_log_mail -> Test with nothing written to log file and mail.
-        test_mail_log_file -> Test with log file and mail.
-        test_log_file -> Test with log file with data.
-        test_empty_log -> Test with nothing written to log file.
+        test_missing_value -> Test with missing -o value.
+        test_missing_option -> Test with missing -o option.
+        test_failure -> Test with failure of mongo dump.
         test_db_dump -> Test with database dump successful.
-        tearDown -> Cleanup of testing environment.
 
     """
 
@@ -220,151 +126,58 @@ class UnitTest(unittest.TestCase):
         """
 
         self.server = Server()
-        self.subp = SubProcess()
-        self.mail = Mail()
-        self.dir_path = "./test/unit/mongo_db_dump/tmp"
-        self.args_array = {"-o": self.dir_path, "-p": "DirectoryPath2"}
-        self.args_array2 = {"-o": self.dir_path, "-p": "DirectoryPath2",
-                            "-x": True}
-        self.file_list = ["2020-08-14T14:31:12 writing sysmon.mysql_perf to",
-                          "2020-08-14T14:31:12 writing sysmon.mongo_rep to"]
+        self.args_array = {"-b": "Database_Name", "-t": "Table_Name",
+                           "-o": "/directory/path"}
+        self.args_array2 = {"-b": "Database_Name", "-t": "Table_Name"}
+        self.args_array3 = {"-b": "Database_Name", "-t": "Table_Name",
+                            "-o": None}
 
-    @mock.patch("mongo_db_dump.gen_libs.is_empty_file",
-                mock.Mock(return_value=False))
-    @mock.patch("mongo_db_dump.gen_libs.file_2_list")
-    @mock.patch("mongo_db_dump.subprocess.Popen")
-    @mock.patch("mongo_db_dump.mongo_libs.create_cmd")
-    def test_mail_log_suppress(self, mock_cmd, mock_subp, mock_file):
+    def test_missing_value(self):
 
-        """Function:  test_mail_log_suppress
+        """Function:  test_missing_value
 
-        Description:  Test with log file and mail and suppression.
+        Description:  Test with missing -o value.
 
         Arguments:
 
         """
 
-        mock_cmd.return_value = "DumpCommand"
-        mock_subp.return_value = self.subp
-        mock_file.return_value = self.file_list
+        self.assertEqual(
+            (mongo_db_dump.mongo_dump(self.server, self.args_array3)),
+            (True, "Error:  Missing -o option or value."))
 
-        with gen_libs.no_std_out():
-            self.assertEqual((mongo_db_dump.mongo_dump(
-                self.server, self.args_array2, mail=self.mail)), (False, None))
+    def test_missing_option(self):
 
-        self.assertEqual(self.mail.data, self.file_list[1])
+        """Function:  test_missing_option
 
-    @mock.patch("mongo_db_dump.gen_libs.is_empty_file",
-                mock.Mock(return_value=False))
-    @mock.patch("mongo_db_dump.gen_libs.file_2_list")
-    @mock.patch("mongo_db_dump.subprocess.Popen")
-    @mock.patch("mongo_db_dump.mongo_libs.create_cmd")
-    def test_log_file_suppress(self, mock_cmd, mock_subp, mock_file):
-
-        """Function:  test_log_file_suppress
-
-        Description:  Test with log file with data and suppression.
+        Description:  Test with missing -o option.
 
         Arguments:
 
         """
 
-        mock_cmd.return_value = "DumpCommand"
-        mock_subp.return_value = self.subp
-        mock_file.return_value = self.file_list
+        self.assertEqual(
+            (mongo_db_dump.mongo_dump(self.server, self.args_array2)),
+            (True, "Error:  Missing -o option or value."))
+
+    @mock.patch("mongo_db_dump.mongo_generic")
+    def test_failure(self, mock_cmd):
+
+        """Function:  test_failure
+
+        Description:  Test with failure of mongo dump.
+
+        Arguments:
+
+        """
+
+        mock_cmd.return_value = (True, "Error Message")
 
         self.assertEqual((mongo_db_dump.mongo_dump(
-            self.server, self.args_array2)), (False, None))
+            self.server, self.args_array)), (True, "Error Message"))
 
-    @mock.patch("mongo_db_dump.subprocess.Popen")
-    @mock.patch("mongo_db_dump.mongo_libs.create_cmd")
-    def test_empty_log_mail(self, mock_cmd, mock_subp):
-
-        """Function:  test_empty_log_mail
-
-        Description:  Test with nothing written to log file and mail.
-
-        Arguments:
-
-        """
-
-        mock_cmd.return_value = "DumpCommand"
-        mock_subp.return_value = self.subp
-
-        self.assertEqual((mongo_db_dump.mongo_dump(
-            self.server, self.args_array, mail=self.mail)), (False, None))
-
-        self.assertEqual(self.mail.data, None)
-
-    @mock.patch("mongo_db_dump.gen_libs.is_empty_file",
-                mock.Mock(return_value=False))
-    @mock.patch("mongo_db_dump.gen_libs.file_2_list")
-    @mock.patch("mongo_db_dump.subprocess.Popen")
-    @mock.patch("mongo_db_dump.mongo_libs.create_cmd")
-    def test_mail_log_file(self, mock_cmd, mock_subp, mock_file):
-
-        """Function:  test_mail_log_file
-
-        Description:  Test with log file and mail.
-
-        Arguments:
-
-        """
-
-        mock_cmd.return_value = "DumpCommand"
-        mock_subp.return_value = self.subp
-        mock_file.return_value = self.file_list
-
-        with gen_libs.no_std_out():
-            self.assertEqual((mongo_db_dump.mongo_dump(
-                self.server, self.args_array, mail=self.mail)), (False, None))
-
-        self.assertEqual(self.mail.data, self.file_list[1])
-
-    @mock.patch("mongo_db_dump.gen_libs.is_empty_file",
-                mock.Mock(return_value=False))
-    @mock.patch("mongo_db_dump.gen_libs.file_2_list")
-    @mock.patch("mongo_db_dump.subprocess.Popen")
-    @mock.patch("mongo_db_dump.mongo_libs.create_cmd")
-    def test_log_file(self, mock_cmd, mock_subp, mock_file):
-
-        """Function:  test_log_file
-
-        Description:  Test with log file with data.
-
-        Arguments:
-
-        """
-
-        mock_cmd.return_value = "DumpCommand"
-        mock_subp.return_value = self.subp
-        mock_file.return_value = self.file_list
-
-        with gen_libs.no_std_out():
-            self.assertEqual((mongo_db_dump.mongo_dump(
-                self.server, self.args_array)), (False, None))
-
-    @mock.patch("mongo_db_dump.subprocess.Popen")
-    @mock.patch("mongo_db_dump.mongo_libs.create_cmd")
-    def test_empty_log(self, mock_cmd, mock_subp):
-
-        """Function:  test_empty_log
-
-        Description:  Test with nothing written to log file.
-
-        Arguments:
-
-        """
-
-        mock_cmd.return_value = "DumpCommand"
-        mock_subp.return_value = self.subp
-
-        self.assertEqual((mongo_db_dump.mongo_dump(
-            self.server, self.args_array)), (False, None))
-
-    @mock.patch("mongo_db_dump.subprocess.Popen")
-    @mock.patch("mongo_db_dump.mongo_libs.create_cmd")
-    def test_db_dump(self, mock_cmd, mock_subp):
+    @mock.patch("mongo_db_dump.mongo_generic")
+    def test_db_dump(self, mock_cmd):
 
         """Function:  test_db_dump
 
@@ -374,27 +187,10 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        mock_cmd.return_value = "DumpCommand"
-        mock_subp.return_value = self.subp
+        mock_cmd.return_value = (False, None)
 
         self.assertEqual((mongo_db_dump.mongo_dump(
             self.server, self.args_array)), (False, None))
-
-    def tearDown(self):
-
-        """Function:  tearDown
-
-        Description:  Cleanup of testing environment.
-
-        Arguments:
-
-        """
-
-        f_list = gen_libs.list_filter_files(self.dir_path, "dump_log_file_*")
-
-        if f_list:
-            for line in f_list:
-                os.remove(line)
 
 
 if __name__ == "__main__":
