dde844889ab78b00bc0ff1a5db4c305e9ccbb2d0
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2022-04-05 16:02:13 +0100

    Update mongo_db_dump.py v3.2.1

--- mongo_db_dump.py
+++ mongo_db_dump.py
@@ -30,20 +30,17 @@
                 -t table => Collection name.
                     -r => Include user and roles in dump.
             -q => Turn quiet mode on. By default, displays out log of dump.
-            -o dir_path => Directory path to dump directory. Required argument
-                for option.
+            -o dir_path => Directory path to dump directory. Required argument.
 
         -A => Run the Sync/Copy dump program. Database server being dumped must
                 also be part of a replica set.
-            -o dir_path => Directory path to dump directory. Required argument
-                for option.
+            -o dir_path => Directory path to dump directory. Required argument.
 
         -E => Run the mongoexport program.
             -b database => Database name.
                 -t table => Collection name.
             -q => Turn quiet mode on. By default, displays out log of dump.
-            -o dir_path => Directory path to dump directory. Required argument
-                for option.
+            -o dir_path => Directory path to dump directory. Required argument.
 
         -e email_address(es) => Send output to one or more email addresses.
         -s subject_line => Subject line of email.
@@ -60,8 +57,8 @@
             configuration file format for the Mongo connection used for
             dumping data from a database.
 
-        Note:  Leave the Mongo replica set entries set to None as it is not
-            required for dumping purposes.
+        Leave the Mongo replica set entries set to None as it is not required
+            for dumping purposes.
 
             Configuration file for Mongo Database Server connection.
 
@@ -77,8 +74,8 @@
             use_arg = True
             use_uri = False
 
-        Note:  If using SSL connections then set one or more of the following
-            entries.  This will automatically enable SSL connections.
+        If using SSL connections then set one or more of the following entries.
+            This will automatically enable SSL connections.
 
             Configuration settings for SSL connections.  See configuration file
                 for details on each entry:
@@ -91,8 +88,20 @@
         Configuration modules -> Name is runtime dependent as it can be used to
             connect to different databases with different names.
 
-    Example:
+        A log file and error file (if any errors are detected) will be
+            written to the same directory as the dump file (-o option).
+
+            Note:  If the -q option is used on the command line, then there
+                will be no log entries in the log file of the dump.
+                To include the log entries, but still have nothing displayed
+                to standard out, then use the "1>/dev/null" after the dump
+                command on the command line.  See example 2 below.
+
+    Example 1:
         mongo_db_dump.py c mongo -d config -o /db_dump -z -M -l
+
+    Example 2: Send log entries to file, but have nothing displayed to stdout.
+        mongo_db_dump.py c mongo -d config -o /db_dump -z -M -l 1>/dev/null
 
 """
 
@@ -260,12 +269,11 @@ def mongo_generic(server, args_array, cmd_name, log_file, **kwargs):
 
     with open(log_file, "w") as l_file:
         proc1 = subp.Popen(
-            cmd, stderr=e_file, stdin=proc2.stdout, stdout=l_file)
+            cmd, stderr=l_file, stdin=proc2.stdout, stdout=e_file)
         proc1.wait()
 
     e_file.close()
 
-# Do I want to set err_flag and err_msg if err_file has something?
     if not gen_libs.is_empty_file(log_file):
         log_list = gen_libs.file_2_list(log_file)
 
@@ -282,6 +290,8 @@ def mongo_generic(server, args_array, cmd_name, log_file, **kwargs):
 
     else:
         err_list = gen_libs.file_2_list(err_file)
+        err_flag = True
+        err_msg = "Error detected in error file: %s" % err_file
 
         if mail:
             mail.add_2_msg("Error messages detected during dump:")
